416650863d1f6726629f46a67182ec003e26cef7
munene mwaniki <nenemwaniki@gmail.com>
2026-04-30 13:25:55 +0300

    Chore:update files before pull

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jaki.demo.com',
+  appName: 'jaki',
+  webDir: 'dist'
+};
+
+export default config;
